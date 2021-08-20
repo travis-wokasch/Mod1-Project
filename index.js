f1b7c9dca4a0fc547e1e2542e4986f1080fd3323
Travis <tjwokasch@gmail.com>
2021-08-20 09:36:42 -0600

    Completed Project for Mod1

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     getBreweries('golden')
 })
 
+
 function getBreweries(location) {
+    let breweryArray = []
     fetch(`https://api.openbrewerydb.org/breweries?by_city=${location}&per_page=50`, {
         method: 'GET',
         headers: {
@@ -10,19 +12,24 @@ function getBreweries(location) {
             'Accept': 'application/json'
         }
     }).then(res =>  res.json())
-    .then(breweries => breweries.map(brewery => {
-        let li = document.createElement('li')
-        let p = document.createElement('p')
-        p.setAttribute('id',`${brewery.id}`)
-        p.textContent = brewery.name
-        let favBtn = document.createElement('button')
-        favBtn.addEventListener('click', addToFavList)
-        favBtn.textContent = 'Add'
-        li.appendChild(p)
-        li.appendChild(favBtn)
-        document.querySelector('#breweries-list').appendChild(li)
-    }))
+    .then(breweries => mapBreweries(breweries))
 }
+
+function mapBreweries(breweryArray) {
+    document.querySelector('#breweries-list').innerHTML = ''
+    breweryArray.map(brewery => {
+    let li = document.createElement('li')
+    let p = document.createElement('p')
+    p.setAttribute('id',`${brewery.id}`)
+    p.textContent = brewery.name
+    let favBtn = document.createElement('button')
+    favBtn.addEventListener('click', addToFavList)
+    favBtn.textContent = 'Add'
+    li.appendChild(p)
+    li.appendChild(favBtn)
+    document.querySelector('#breweries-list').appendChild(li)
+    })
+} 
 
 let form = document.querySelector('form')
     form.addEventListener('submit', (e) => {
@@ -34,7 +41,7 @@ let form = document.querySelector('form')
 function addToFavList(e) {
     let favList = document.querySelector('#favorite-list')
     let li = document.createElement('li')
-    let breweryName = e.target.parentNode.innerText
+    let breweryName = e.target.parentNode.querySelector('p').innerText
     li.textContent = breweryName
     let btn = document.createElement('button')
     btn.addEventListener('click', btnDelete)
